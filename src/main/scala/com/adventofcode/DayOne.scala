23cb2925d64a189486529fda4f040f1a519888bc
Mario Castro Squella <mario.castro.squella@gmail.com>
2016-04-25 16:12:49 -0300

    Solutions for Part Two of Day One.

--- src/main/scala/com/adventofcode/DayOne.scala
+++ src/main/scala/com/adventofcode/DayOne.scala
@@ -1,5 +1,8 @@
 package com.adventofcode
 
+import java.io.InputStream
+import scala.annotation.tailrec
+
 /**
  * Created by mcastro on 04-04-2016.
  */
@@ -12,6 +15,25 @@ class DayOne {
   }
 
   def partTwo(input:String): Int = {
-  	0
+    @tailrec
+    def partTwoRec(in: List[Int], acc: (Int, Int)): Int = {
+      if (acc._1 + in.head == -1) acc._2
+      else partTwoRec(in.tail, (acc._1 + in.head, acc._2 + 1))
+    }
+    partTwoRec(input.toList.map{
+      case '(' => 1
+      case ')' => -1}, (0, 1))
+  }
+}
+
+object DayOne {
+  val stream: InputStream = getClass.getClassLoader.getResourceAsStream("DayOneInput.txt")
+  val input = scala.io.Source.fromInputStream(stream).getLines.mkString
+
+  def main(args: Array[String]): Unit = {
+    val dayOne = new DayOne
+    println("--- Day 1: Not Quite Lisp ---")
+    println("Answer to Part One is: " + dayOne.partOne(input))
+    println("Answer to Part Two is: " + dayOne.partTwo(input))
   }
 }
