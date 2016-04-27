27853111efa50ac256678203d438ec4eca68ebfe
Mario Castro Squella <mario.castro.squella@gmail.com>
2016-04-27 13:59:17 -0300

    Added the functions needed for hashing on the Day Four problem.

--- src/main/scala/com/adventofcode/DayFour.scala
+++ src/main/scala/com/adventofcode/DayFour.scala
@@ -1,5 +1,8 @@
 package com.adventofcode
 
+import java.security.MessageDigest
+import java.lang.Byte
+
 /**
  * Created by mcastro on 27-04-2016.
  */
@@ -7,6 +10,17 @@ class DayFour {
   def partOne(secretKey: String): Int = {
     0
   }
+
+  // Obtained from http://stackoverflow.com/questions/5992778/computing-the-md5-hash-of-a-string-in-scala
+  def md5(s: String) = {
+    MessageDigest.getInstance("MD5").digest(s.getBytes)
+  }
+
+  // Adapted from http://www.jroller.com/thebugslayer/entry/playing_with_bytes_and_hex
+  def bytesToHex(bytes : List[Byte]) = {
+    bytes.map{ b => String.format("%02X", Byte.valueOf(b)) }.mkString.toLowerCase
+  }
+
 }
 
 object DayFour {
