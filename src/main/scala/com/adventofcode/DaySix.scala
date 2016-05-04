aead71a776e213b91b1786ec7021956bb20cdcdf
Mario Castro Squella <mario.castro.squella@gmail.com>
2016-05-04 00:25:50 -0300

    Solution and tests for Part Two of Day Six.

--- src/main/scala/com/adventofcode/DaySix.scala
+++ src/main/scala/com/adventofcode/DaySix.scala
@@ -7,8 +7,33 @@ import java.io.InputStream
  */
 class DaySix {
   case class Light(x: Int, y: Int, on: Boolean)
+  case class BrightLight(x: Int, y: Int, brightness: Int)
 
   def executeInstruction(instruction: String, lights: Array[Array[Light]]): Array[Array[Light]] = {
+    instruction.split(" ").toList match {
+      case List("turn", "off", from, "through", to) => {
+        for {
+          i <- from.split(",")(0).toInt to to.split(",")(0).toInt
+          j <- from.split(",")(1).toInt to to.split(",")(1).toInt
+        } lights(i)(j) = turnOff(lights(i)(j))
+      }
+      case List("turn", "on", from, "through", to) => {
+        for {
+          i <- from.split(",")(0).toInt to to.split(",")(0).toInt
+          j <- from.split(",")(1).toInt to to.split(",")(1).toInt
+        } lights(i)(j) = turnOn(lights(i)(j))
+      }
+      case List("toggle", from, "through", to) => {
+        for {
+          i <- from.split(",")(0).toInt to to.split(",")(0).toInt
+          j <- from.split(",")(1).toInt to to.split(",")(1).toInt
+        } lights(i)(j) = toggle(lights(i)(j))
+      }
+    }
+    lights
+  }
+
+  def executeInstruction2(instruction: String, lights: Array[Array[BrightLight]]): Array[Array[BrightLight]] = {
     instruction.split(" ").toList match {
       case List("turn", "off", from, "through", to) => {
         for {
@@ -40,9 +65,21 @@ class DaySix {
     } yield 1).foldLeft(0)((a, b) => a+b)
   }
 
+  def countTotalBrightness(lights: Array[Array[BrightLight]]): Int = {
+    (for {
+      i <- 0 until lights.size
+      j <- 0 until lights(0).size
+    } yield lights(i)(j).brightness).foldLeft(0)((a, b) => a + b)
+  }
+
   def toggle(light: Light): Light = {
     Light(light.x, light.y, !light.on)
   }
+
+  def toggle(light: BrightLight): BrightLight = {
+    BrightLight(light.x, light.y, light.brightness + 2)
+  }
+
 
   def turnOn(light: Light): Light = {
     light match {
@@ -51,10 +88,21 @@ class DaySix {
     }
   }
 
+  def turnOn(light: BrightLight): BrightLight = {
+    BrightLight(light.x, light.y, light.brightness + 1)
+  }
+
   def turnOff(light: Light): Light = {
     light match {
       case Light(_, _, true) => toggle(light)
       case _ => light
+    }
+  }
+
+  def turnOff(light: BrightLight): BrightLight = {
+    light.brightness match {
+      case 0 => light
+      case _ => BrightLight(light.x, light.y, light.brightness - 1)
     }
   }
 
@@ -66,6 +114,15 @@ class DaySix {
     } lightArray(i)(j) = Light(i, j, false)
     lightArray
   }
+
+  def initializeBrightArray(n: Int, m: Int): Array[Array[BrightLight]] = {
+    val lightArray = Array.ofDim[BrightLight](n, m)
+    for {
+      i <- 0 until n
+      j <- 0 until m
+    } lightArray(i)(j) = BrightLight(i, j, 0)
+    lightArray
+  }
 }
 
 object DaySix {
@@ -75,10 +132,15 @@ object DaySix {
   def main(args: Array[String]): Unit = {
     val daySix = new DaySix
     val lights = daySix.initializeArray(1000, 1000)
+    val brightLights = daySix.initializeBrightArray(1000, 1000)
     println("--- Day 6: Probably a Fire Hazard ---")
     for {
       line <- input
-    } daySix.executeInstruction(line, lights)
+    } {
+      daySix.executeInstruction(line, lights)
+      daySix.executeInstruction2(line, brightLights)
+    }
     println("Answer to Part One is: " + daySix.countTurnedOnLights(lights))
+    println("Answer to Part Two is: " + daySix.countTotalBrightness(brightLights))
   }
 }
--- src/test/scala/com/adventofcode/DaySixSpec.scala
+++ src/test/scala/com/adventofcode/DaySixSpec.scala
@@ -13,9 +13,17 @@ class DaySixSpec extends FlatSpec with Matchers {
     daySix.toggle(daySix.Light(0,0,false)) shouldEqual daySix.Light(0,0,true)
   }
 
+  "toggle()" should "increase brightness of a BrightLight by 2" in {
+    daySix.toggle(daySix.BrightLight(0, 0, 1)) shouldEqual daySix.BrightLight(0, 0, 3)
+  }
+
   "turnOn()" should "turn on the light only if it's off" in {
     daySix.turnOn(daySix.Light(0,0,true)) shouldEqual daySix.Light(0,0,true)
     daySix.turnOn(daySix.Light(0,0,false)) shouldEqual daySix.Light(0,0,true)
+  }
+
+  "turnOn()" should "increase brightness of a BrightLight by 1" in {
+    daySix.turnOn(daySix.BrightLight(0, 0, 1)) shouldEqual daySix.BrightLight(0, 0, 2)
   }
 
   "turnOff()" should "turn off the light only if it's on" in {
@@ -23,9 +31,20 @@ class DaySixSpec extends FlatSpec with Matchers {
     daySix.turnOff(daySix.Light(0,0,false)) shouldEqual daySix.Light(0,0,false)
   }
 
+  "turnOff()" should "decrease the brightness of a BrightLight by 1 until it reaches 0" in {
+    daySix.turnOff(daySix.BrightLight(0,0,2)) shouldEqual daySix.BrightLight(0,0,1)
+    daySix.turnOff(daySix.BrightLight(0,0,1)) shouldEqual daySix.BrightLight(0,0,0)
+    daySix.turnOff(daySix.BrightLight(0,0,0)) shouldEqual daySix.BrightLight(0,0,0)
+  }
+
   "initializeArray()" should "initialize an array of lights that are turned off" in {
     daySix.initializeArray(1, 1) shouldEqual Array(Array(daySix.Light(0,0, false)))
     daySix.initializeArray(2, 2) shouldEqual Array(Array(daySix.Light(0,0, false), daySix.Light(0,1, false)), Array(daySix.Light(1,0, false), daySix.Light(1,1, false)))
+  }
+
+  "initializeBrightArray()" should "initialize an array of lights that have zero brightness" in {
+    daySix.initializeBrightArray(1, 1) shouldEqual Array(Array(daySix.BrightLight(0,0, 0)))
+    daySix.initializeBrightArray(2, 2) shouldEqual Array(Array(daySix.BrightLight(0,0, 0), daySix.BrightLight(0,1, 0)), Array(daySix.BrightLight(1,0, 0), daySix.BrightLight(1,1, 0)))
   }
 
   "executeInstruction()" should "apply the instruction to the lights array" in {
@@ -36,12 +55,28 @@ class DaySixSpec extends FlatSpec with Matchers {
     daySix.executeInstruction("toggle 0,0 through 0,1", lights) shouldEqual Array(Array(daySix.Light(0,0, false), daySix.Light(0,1, true)), Array(daySix.Light(1,0, true), daySix.Light(1,1, false)))
   }
 
+  "executeInstruction2()" should "apply the instruction to the lights array" in {
+    val lights = daySix.initializeBrightArray(2, 2)
+
+    daySix.executeInstruction2("turn on 0,0 through 1,1", lights) shouldEqual Array(Array(daySix.BrightLight(0,0, 1), daySix.BrightLight(0,1, 1)), Array(daySix.BrightLight(1,0, 1), daySix.BrightLight(1,1, 1)))
+    daySix.executeInstruction2("turn off 0,1 through 1,1", lights) shouldEqual Array(Array(daySix.BrightLight(0,0, 1), daySix.BrightLight(0,1, 0)), Array(daySix.BrightLight(1,0, 1), daySix.BrightLight(1,1, 0)))
+    daySix.executeInstruction2("toggle 0,0 through 0,1", lights) shouldEqual Array(Array(daySix.BrightLight(0,0, 3), daySix.BrightLight(0,1, 2)), Array(daySix.BrightLight(1,0, 1), daySix.BrightLight(1,1, 0)))
+  }
+
   "countTurnedOnLights" should "correctly count the number of lights that are turned on" in {
     val lights = daySix.initializeArray(2, 2)
     daySix.executeInstruction("turn on 0,0 through 1,1", lights)
     daySix.countTurnedOnLights(lights) shouldEqual 4
     daySix.executeInstruction("turn off 0,1 through 1,1", lights)
     daySix.countTurnedOnLights(lights) shouldEqual 2
+  }
+
+  "countTotalBrightness" should "correctly calculate the total brightness of the lights array" in {
+    val lights = daySix.initializeBrightArray(2, 2)
+    daySix.executeInstruction2("turn on 0,0 through 1,1", lights)
+    daySix.countTotalBrightness(lights) shouldEqual 4
+    daySix.executeInstruction2("turn off 0,1 through 1,1", lights)
+    daySix.countTotalBrightness(lights) shouldEqual 2
   }
 
 }
